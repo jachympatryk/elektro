e3471e41719f206acbb7899eb7e607e5d0652460
patrykjachym <jachympatryk99@gmail.com>
2023-01-11 19:13:19 +0100

    services data

---
+++ src/pages/service/service.constants.ts
@@ -0,0 +1,22 @@
+import service1 from "assets/images/service1.jpg";
+import service2 from "assets/images/service2.jpg";
+import service3 from "assets/images/service3.jpg";
+
+export const serviceData = [
+  {
+    image: service1,
+    label: "Connection",
+    content: "Power connections, string efficiency. Earth measurements, tightness and power efficiency.",
+  },
+  {
+    image: service2,
+    label: "Aesthetics and quality",
+    content:
+      "Aesthetics, apart from the appearance, affects the durability of the installations. Free electricity is to be used for many many years.",
+  },
+  {
+    image: service3,
+    label: "Production analysis",
+    content: "We analyze demand, energy, consumption and production by individual subassemblies and components.",
+  },
+];
