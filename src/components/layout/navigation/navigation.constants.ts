346feb3f89d00bcc9da2feefb1eccac716e0a1ac
patrykjachym <jachympatryk99@gmail.com>
2023-01-14 11:49:27 +0100

    navigation

---
+++ src/components/layout/navigation/navigation.constants.ts
@@ -0,0 +1,22 @@
+export const navigationData = [
+  {
+    path: "/",
+    label: "Home",
+  },
+  {
+    path: "/portfolio",
+    label: "Portfolio",
+  },
+  {
+    path: "/contact",
+    label: "Contact",
+  },
+  {
+    path: "/service",
+    label: "Service",
+  },
+  {
+    path: "/career",
+    label: "Career",
+  },
+];
--- src/components/layout/navigation/navigation.tsx
+++ src/components/layout/navigation/navigation.tsx
@@ -1,7 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, IconButton, Typography } from "@mui/material";
+import { Link, useLocation } from "react-router-dom";
+import MenuIcon from "@mui/icons-material/Menu";
+import { useDidUpdate } from "@better-typed/react-lifecycle-hooks";
+import classNames from "classnames";
+
+import { navigationData } from "./navigation.constants";
+import { useWindowSize } from "hooks";
 
 import styles from "./navigation.module.scss";
 
 export const Navigation = () => {
-  return <div className={styles.container}>navigation</div>;
+  const { width } = useWindowSize();
+  const { pathname } = useLocation();
+  const [isMenuOpen, setIsMenuOpen] = useState(true);
+
+  const handleMenu = () => {
+    setIsMenuOpen((prevState) => !prevState);
+  };
+
+  const isMobile = width < 997;
+
+  useDidUpdate(() => {
+    if (isMobile && isMenuOpen) setIsMenuOpen(false);
+  }, [pathname]);
+
+  useDidUpdate(() => {
+    if (isMobile) setIsMenuOpen(false);
+    if (!isMobile && isMenuOpen) setIsMenuOpen(false);
+  }, [isMobile]);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <Typography className={styles.logo}>ELEKTRO</Typography>
+
+        {isMobile && (
+          <IconButton onClick={handleMenu}>
+            <MenuIcon />
+          </IconButton>
+        )}
+      </div>
+      {isMenuOpen && isMobile && (
+        <div className={styles.menuMobile}>
+          <div className={styles.links}>
+            {navigationData.map(({ path, label }) => {
+              const activePath = path === pathname;
+              return (
+                <Link className={classNames(styles.link, { [styles.activeLink]: activePath })} to={path}>
+                  {label}
+                </Link>
+              );
+            })}
+            <Button className={styles.button}>Partnership</Button>
+          </div>
+        </div>
+      )}
+      {!isMobile && (
+        <div className={styles.menuDesktop}>
+          {navigationData.map(({ path, label }) => {
+            const activePath = path === pathname;
+            return (
+              <Link className={classNames(styles.link, { [styles.activeLink]: activePath })} to={path}>
+                {label}
+              </Link>
+            );
+          })}
+          <Button className={styles.button}>Partnership</Button>
+        </div>
+      )}
+    </div>
+  );
 };
